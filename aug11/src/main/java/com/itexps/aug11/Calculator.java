b34568a5f770cf4e5a4887416bba411dbb4ba7ff
vaishali0606 <52891296+vaishali0606@users.noreply.github.com>
2019-08-11 10:35:31 -0500

    initial project
    initial project

---
+++ aug11/src/main/java/com/itexps/aug11/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.aug11;
+
+/**
+ *
+ * @author Vaishali
+ */
+public class Calculator {
+    public int add (int x, int y){
+            
+        return x + y;
+    }
+    public int sub (int x,int y){
+        return x-y;
+    }
+}
---
+++ aug11/src/test/java/test/com/CalculatorTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.itexps.aug11.Calculator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Vaishali
+ */
+public class CalculatorTest {
+    Calculator cal;
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        cal = new Calculator();
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testAdd(){
+        int z = cal.add(4,5);
+        assertEquals(9,z);
+    } 
+    @Test
+    public void testSub(){
+        int z = cal.sub(4,5);
+        assertEquals(-1,z);    
+    }
+}
